a5e55b3a499f0a600e7410d356f476f28bd5fc6f
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-12-09 11:32:59 +0700

    function as data type

--- Unit2/1.Kotlin-fundamental/Trickortreat.kt
+++ Unit2/1.Kotlin-fundamental/Trickortreat.kt
@@ -1,13 +1,32 @@
 fun main(){
-    val trickFunction = trick
-    trick()
+    val coins: (Int) -> String = { quantity ->
+        "$quantity quarters"
+    }
+
+    val treatFunction = TrickOrTreat(false, coins)
+    val trickFunction = TrickOrTreat(true, null)
+    
+    treatFunction()
     trickFunction()
+
+}
+
+fun TrickOrTreat (isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick) {
+        return trick
+    } else {
+        if (extraTreat != null){
+          println(extraTreat(5))  
+        }
+        
+        return treat
+    }
 }
 
 val trick = {
     println("No treats!")
 }
 
-fun treat(){
-    println("treat")
+val treat: () -> Unit = {
+    println("Have a treat")
 }
